7fecde65fa5a64154727e4cb75c0ae5c99a80a93
anishasinghrajput7656-ui <anishasinghrajput7656@gmail.com>
2026-04-20 19:21:23 +0530

    add img on images folder

--- videocall/settings.py
+++ videocall/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
+    # 'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
+    # 'channels',
     'call',
     'bootstrap5',
 ]
